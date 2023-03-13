211d5cae2805e9de0d6af456fc2674e411305bab
Matthieu Mechineau <mattherb@live.fr>
2023-03-13 07:11:08 +0100

    ehe

--- bruit.js
+++ bruit.js
@@ -1,84 +1,43 @@
-const speed_ini = 25;
-let speed = speed_ini;
-const canvasW = 800;
-const canvasH = 800;
 
-let joueur = {
-  'x' : 400, 'y' : 400, 'w' : 50, 'h' : 50
-};
+const canvasW = 768;
+const canvasH = 512;
 
 
-let sol = { //sol
-  'x1' : 0, 'x2' : canvasW, 'y1' : canvasH, 'y2' : canvasH, 'nature' : 'sol'
-}; 
-let obstacle_rect_1 ={ //rect
-  'x' : canvasW/2, 'y': canvasH-100, 'w':100, 'h': 100, 'nature' : 'rect'
-}
-let ensemble_collision = [sol,obstacle_rect_1];
-
-function collision(tab_murs,joueur){
-  let center_x = joueur.x + joueur.w/2;
-  let center_y = joueur.y + joueur.h/2;
-  tab_murs.forEach(element => {
-    if(element.nature==='sol' && center_y > element.y1-joueur.h/2){
-      joueur.y = element.y1 -joueur.h;
-    }
-    if(element.nature ==='rect'){
-      
-    }
-  });
+function fields(density , tab ) //lvl 0 = superfields ; 1 = large ; 2 = small ; 3 = nano ; 4 = raster
+{
+  this.density = +density ;
+  this.tab = tab ;
 }
 
-function setup() {
-  print('Hello World');
+function tab(nextField , array )
+{
+  this.nextField = nextField ;
+  this.array = array;    
+}
+
+
+
+
+  
+  
+
+  
+  
+  
+  
+  
+function test(field)
+{
+
+}
+
+
+function setup()
+{
   createCanvas(canvasW,canvasH);
-  
 }
+function draw()
+{
+  background(255);
 
-// function keyPressed()
-// {
-//   if(keyCode === UP_ARROW
-// }
-
-function draw() {
-    
-  
-  background(255); //valeur entre 0 et 255, niveaux de gris
-  fill(0,0,0);
-  let isPressed;
-  if(keyIsPressed === true){
-    isPressed = true;
-    if(keyIsDown(LEFT_ARROW)){
-      joueur.x+=-speed;
-      if(joueur.x < -25){
-        joueur.x=824;
-      }
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-      joueur.x+=speed;
-      if(joueur.x > 825){
-        joueur.x=-24;
-      }
-    }
-    if(keyIsDown(UP_ARROW)){
-      joueur.y+=-speed;
-      if(joueur.y < -25){
-        joueur.y=824;
-      }
-    }
-    if(keyIsDown(DOWN_ARROW)){
-      joueur.y+=speed;
-      if(joueur.y > 825){
-        joueur.y=-24;
-      }
-    }
-    speed += 0.001 * speed;
-  }else{
-    speed = speed_ini;
-    isPressed = false;
-  }
-  collision(ensemble_collision,joueur);
-  rect(joueur.x,joueur.y,joueur.w,joueur.h);
-  frameRate(120);
-  
 }
