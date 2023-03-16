e67787a081282d928344651236527e70d17b3514
Matthieu Mechineau <mattherb@live.fr>
2023-03-16 14:31:34 +0100

    Tiens florian

--- bruit copy.js
+++ bruit copy.js
@@ -26,36 +26,122 @@ for(let i =0 ; i<16 ; i++)
   {
     let i_removed = Math.floor(Math.random()*tabDensityRandom_largeField.length-1);
     superField[i].nextField[j] = new item(new Array(16),tabDensityRandom_largeField.splice(i_removed,1));
+    let tabDensityRandom_mediumField = [hDens , hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens, mDens ,mDens ,mDens ,sDens, sDens ,nDens];
+    for(let k = 0 ; k<16 ; k++)
+    {
+      let i_removed = Math.floor(Math.random()*tabDensityRandom_mediumField.length-1);
+      superField[i].nextField[j].nextField[k] = new item(new Array(16),tabDensityRandom_mediumField.splice(i_removed,1));
 
+      let tabDensityRandom_smallField = [hDens , hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens, mDens ,mDens ,mDens ,sDens, sDens ,nDens];
+      for(let l = 0 ; l<16 ; l++)
+      {
+        let i_removed = Math.floor(Math.random()*tabDensityRandom_smallField.length-1);
+        superField[i].nextField[j].nextField[k].nextField[l] = new item(new Array(6),tabDensityRandom_smallField.splice(i_removed,1));
+
+        let tabDensityRandom_modules = [hDens , hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens ,hDens, mDens ,mDens ,mDens ,sDens, sDens ,nDens]; ;
+        for(let m = 0 ; m<6 ; m++)
+        {
+          let i_removed = Math.floor(Math.random()*tabDensityRandom_modules.length-1);
+          superField[i].nextField[j].nextField[k].nextField[l].nextField[m] = new item([],tabDensityRandom_modules.splice(i_removed,1));
+        }
+      }
+    }
   }
 };
 
 function setup()
 {
   createCanvas(canvasW,canvasH);
+  noLoop();
+  frameRate();
+}
+
+let clickCount = 4;
+
+function mouseClicked()
+{
+  clickCount ++;
   
 }
+
 function draw()
 {
   background(255);
-  let x =0;
-  let y =0;
+  
+
   let i=0;
+  let l=0;
+  let k=0;
+  let j =0;
+
   for(let xi = 0 ; xi<768 ; xi+=192)
   {
     for(let yi = 0 ; yi<512 ; yi+=128)
     {
       i++;
-      
-      let f1 = superField[i];
-      console.log(f1);
-      let p = f1.density; //* f2.density;
-      // let f2 = f1.nextField[j];
-      let placeNoireBoule = random(1000);
-            if(placeNoireBoule<p*1000)
+      console.log(`i=${i}`);
+      if(i==16)
+      {
+        i=0;
+      }
+      for(let xj = 0 ; xj<192 ; xj+= 48)
+      {
+        for(let yj = 0 ; yj<128 ; yj+= 32)
+        {
+          j++;
+          console.log(`j=${j}`);
+          if(j==16)
+          {
+            j=0;
+          }
+          for(let xk = 0 ; xk<48 ; xk+= 12)
+          {
+            for(let yk = 0 ; yk<32 ; yk+= 8)
             {
-              rect(xi,yi,20,20);
-            };      
+              k++;
+              console.log(`k=${k}`);
+              if(k==16)
+              {
+                k=0;
+              }
+              for(let xl = 0 ; xl<12 ; xl+= 3)
+              {
+                for(let yl = 0 ; yl<8 ; yl+= 2)
+                  {
+                    l++;
+                    console.log(`l=${l}`);
+                    if(l==16)
+                    {
+                      l=0;
+                    }
+                    let m = 0;
+                    for(let xm = 0 ; xm<3 ; xm+= 1)
+                    {
+                      for(let ym = 0 ; ym<1 ; ym+= 1)
+                      {
+                        m++;
+                        console.log(`m=${m}`);
+                        let p =0;
+                        let f1 = superField[i];
+                        let f2 = superField[i].nextField[j];
+                        let f3 = superField[i].nextField[j].nextField[k];
+                        let f4 = superField[i].nextField[j].nextField[k].nextField[l];
+                        let f5 = superField[i].nextField[j].nextField[k].nextField[l].nextField[m];
+                        p = f1.density * f2.density *f3.density*f4.density*f5.density;                        
+                        let placeNoireBoule = random(1000);
+                        if(placeNoireBoule<(p*1000))
+                          {
+                            rect(xi+xj+xk+xl+xm,yi+yj+yk+yl+ym,1,1); 
+                          };      
+                      }
+                    }
+                  }
+              }
+            }
+          }
+        }
+      }
+                      
     }
   }
 };
